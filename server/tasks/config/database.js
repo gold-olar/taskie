07de53ecd2b3374ae1ac21ee22140b92cc00cbb4
gold-olar <sam99kupo@gmail.com>
2020-10-13 21:32:35 +0100

    update atlas db url

--- server/tasks/config/database.js
+++ server/tasks/config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://taskie-admin:taskieadmin@taskie.z2xyu.mongodb.net/<dbname>?retryWrites=true&w=majority",
+      "mongodb+srv://taskie-admin:taskieadmin@taskie.z2xyu.mongodb.net/taskie?retryWrites=true&w=majority",
   };
 } else {
   module.exports = { mongoURI: "mongodb://db:27017/taskie" };
